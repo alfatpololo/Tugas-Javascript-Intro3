6b138c525be63c61673f35158da1567923f30ccd
alfatpololo <59309653+alfatpololo@users.noreply.github.com>
2022-09-25 14:12:13 +0700

    Update tugas4.js

--- tugas4.js
+++ tugas4.js
@@ -1,12 +1,15 @@
-async function ambil(){
-    try{
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-        const data = await response.json();
-        console.log(data);
-    }catch(error){
-        console.log("Error fetching API");
-        console.log(error);
-    }
-}
+const fetchAPI = async () => {
+  try {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await resp.json();
 
-ambil();
+    data.map((item) => {
+      console.log(item.title);
+    });
+  } catch (error) {
+    console.log("error data fetch");
+    console.log(error);
+  }
+};
+
+fetchAPI();
